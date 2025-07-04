b155005912762f4629dd295d21c871907c0e9cd2
Pacharapol 'Fluke' Apitanapat <fluke.pacharapol@maqe.com>
2025-07-04 21:21:12 +0700

    add worker mode config

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   projectConfig: {
     redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
-    databaseLogging: true,
+    databaseLogging: false,
+    workerMode: process.env.WORKER_MODE || "server",
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
